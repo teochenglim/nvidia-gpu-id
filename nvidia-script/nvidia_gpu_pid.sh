9e11e136fddf67cb42b45337d0a41792888303b2
Teo Cheng Lim <teochenglim@gmail.com>
2018-06-29 13:10:49 +0800

    added memory usage

--- nvidia-script/nvidia_gpu_pid.sh
+++ nvidia-script/nvidia_gpu_pid.sh
@@ -9,22 +9,23 @@ docker_process_pid=`python children.py $docker_main_pid`
 #echo "docker process pid: $docker_process_pid"
 
 #nvidia-smi pmon -c 1 | egrep -v "gpu|Idx|-" | awk '{print $1":"$2":"}'
-nvidia_pids=`nvidia-smi pmon -c 1 | egrep -v "gpu|Idx|-" | awk '{print $1":"$2":"}'`
+nvidia_pids=`nvidia-smi pmon -c 1 -s m | egrep -v "gpu|Idx|-" | awk '{print $1":"$2":"$4}'`
 #echo "nvidia pids: $nvidia_pids"
 
 
 echo "========================================================================================="
-echo "gpu_id:process_id:docker_container_id"
+echo "gpu_id:process_id:docker_container_id:memory_used(in MB)"
 echo "========================================================================================="
 for i in $nvidia_pids
 do
   #echo $i
   gpu_id=`echo $i | cut -d: -f1`
   pid_id=`echo $i | cut -d: -f2`
+  mem_used=`echo $i | cut -d: -f3` 
   #print $gpu_id
   #print $pid_id
   container_id=`python get_parent.py $pid_id $docker_process_pid`
-  echo "#"$gpu_id":"$pid_id":"$container_id
+  echo "#"$gpu_id":"$pid_id":"$container_id":"$mem_used
 done
 echo "========================================================================================="
 echo "1. use 'nvidia-smi' to find which process is using which GPU"
